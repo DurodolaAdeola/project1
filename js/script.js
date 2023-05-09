77dcdd1e0b15726d3e1fc7e0d1f1ab79fd17ee9e
durodola adeola <durodolaadeola7@gmail.com>
2023-05-09 16:33:12 +0100

    push

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+    $("#Form").submit(function(event){
+    event.preventDefault();
+    const firstname =$("#firstname").val();
+    const laststname =$("#lastname").val();
+    $(".output").text(firstname);
+  
+         
+    })
 
 });
